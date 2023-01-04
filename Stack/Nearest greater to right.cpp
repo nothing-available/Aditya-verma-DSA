52c1541b127bd0e858806e6a225b28b199087474
Sumit kumar <78586294+nothing-available@users.noreply.github.com>
2023-01-05 00:26:57 +0530

    Update Nearest greater to right.cpp

--- Stack/Nearest greater to right.cpp
+++ Stack/Nearest greater to right.cpp
@@ -1,1 +1,45 @@
-
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        
+        vector<long long> ans;
+        stack<long long> st;
+        
+        for(int i = n-1 ; i >= 0 ; i--){
+            
+            if(st.empty()){
+                ans.push_back(-1);
+            }
+            
+            else if(!st.empty() && st.top() > arr[i]){
+                
+                ans.push_back(st.top());
+            }
+            
+            else{
+                
+                while(!(st.top() > arr[i])){
+                    
+                    st.pop();
+                    if(st.empty()){
+                        break;
+                    }
+                }
+                
+                if(!st.empty()){
+                    
+                    ans.push_back(st.top());
+                }
+                else{
+                    ans.push_back(-1);
+                }
+            }
+            st.push(arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    
+};
